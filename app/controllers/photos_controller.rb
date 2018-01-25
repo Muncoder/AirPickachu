a3a76afb45b813f2dc9168074aeb98112f8c4783
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-01-25 19:56:26 +0000

    photo delete by ajax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,4 +12,13 @@ class PhotosController < ApplicationController
 		@photos = @room.photos
 		redirect_back(fallback_location: request.referer, notice: "Saved......")
 	end
+
+	def destroy
+		@photo = Photo.find(params[:id])
+		@room = Room.find(@photo.room_id)
+
+		@photo.destroy
+
+		@photos = @room.photos
+	end
 end
